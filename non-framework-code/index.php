a02597b295d45ecfd38477eec091fa705e32b3c2
JacksonBrown <jacksonconnerbrown@gmail.com>
2016-03-11 23:15:05 -0600

    0-100 real quick

--- non-framework-code/index.php
+++ non-framework-code/index.php
@@ -1,13 +1,33 @@
 <?php
 
-$title = "Vaughn Development and Administration";
-$landing_message = "Hey, We're Vaughn";
-$landing_description = "We develop <span class='light-green'>websites</span>/<span class='light-green'>applications</span>, 
+$title = "I Do Development and Administration";
+$landing_message = "Oh Hi, I'm Jackson";
+$landing_description = "I develop <span class='light-green'>websites</span>/<span class='light-green'>applications</span>, 
 						and administor 
 						<span class='light-green'>Linux</span> and 
 						<span class='light-green'>Windows</span> Systems. <br /> 
-						Need to contact us? Message our github.";
+						Need to contact me? Send an email!";
+$skype = "realtalk1217";
+$phone = "+00 918-931-7486";
+$ouremail = "jacksonconnerbrown@gmail.com";
+$ouraddress = " 106 Tommye Ln<br>
+				Tahlequah, OK 74464<br>
+				United States";
 
+if(isset($_POST['submit'])){
+    	$to = "jacksonconnerbrown@gmail.com"; // this is your Email address
+    	$from = $_POST['mail']; // this is the sender's Email address
+    	$first_name = $_POST['name'];
+    	$subject = "Form submission";
+    	$subject2 = $_POST['subject'];
+    	$message = $first_name . " wrote the following:" . "\n\n" . $_POST['message'];
+    	$message2 = "Here is a copy of your message " . $first_name . "\n\n" . $_POST['message'];
+
+    	$headers = "From:" . $from;
+    	$headers2 = "From:" . $to;
+    	mail($to,$subject,$message,$headers);
+    	mail($from,$subject2,$message2,$headers2); // sends a copy of the message to the sender
+}
 ?>
 
 <!DOCTYPE html>
@@ -32,11 +52,6 @@ $landing_description = "We develop <span class='light-green'>websites</span>/<sp
 <body>
 	<div class="jumbotron home home-fullscreen" id="home">
 		<div class="mask"></div>
-		<!--
-		<a href="#" class="logo">
-			<i class="fa fa-cloud logo-font" style="color: #fff;"></i>
-		</a>
-		-->
 		<a href="" class="menu-toggle" id="nav-expander" style="position: fixed;"><i class="fa fa-bars"></i></a>
 		<!-- Offsite navigation -->
 		<nav class="menu">
@@ -56,41 +71,38 @@ $landing_description = "We develop <span class='light-green'>websites</span>/<sp
 			<div class="header-info">
 				<h1 class="landing-header"><?php echo $landing_message; ?></h1>
 				<p class="landing-description"><?php echo $landing_description; ?></p>
-				<a href="http://github.com/JacksonBrown" class="btn btn-primary" target="_blank">Our Github</a>
+				<a href="#contact" data-scroll class="btn btn-primary" target="_blank">I Don't Bite</a>
 			</div>
 		</div>
 	</div>
 	<!-- Services section start -->
-	<section id="services">
+	<section id="services" style="background-color: #efefef;">
 		<div class="container">
 			<div class="row">
 				<div class="col-md-4">
 					<div class="service-item">
 						<div class="icon"><i class="fa fa-thumbs-up"></i></div>
-						<h3>We Are Professional</h3>
+						<h3>I Administrate Linux Systems</h3>
 						<p>
-						The tasks we do will be accomplished with swift finesse. We accompany to the needs of our clients and put
-						them first.
+						I adminstrate Linux systems, ranging from user configuration and security, to partition schematics and software management.
 						</p>
 					</div>
 				</div>
 				<div class="col-md-4">
 					<div class="service-item">
 						<div class="icon"><i class="fa fa-adjust"></i></div>
-						<h3>Our Work Is Affordable</h3>
+						<h3>I Configrue Linux Servers</h3>
 						<p>
-						Our set price is $12.50 per hour, however we are open to discuss any decrease in the cost. We do not wish to
-						allow our price to detriment relationships with our clients.
+						I configure Linux servers, doing tasks such as IP routing, interface configuration, and server set up and moderating (Apache2, Nginx, Email Servers, DNS Servers, etc).
 						</p>
 					</div>
 				</div>
 				<div class="col-md-4">
 					<div class="service-item">
 						<div class="icon"><i class="fa fa-life-ring"></i></div>
-						<h3>We Love To Help</h3>
+						<h3>I Aid In Developments</h3>
 						<p>
-						We love to aid in projects. We're proficient in web development and python development. We also administor
-						Linux systems and Windows systems.
+						I love to aid in projects. I'm proficient in back end and front end web development and python console/gui application and server development. 
 						</p>
 					</div>
 				</div>
@@ -98,7 +110,8 @@ $landing_description = "We develop <span class='light-green'>websites</span>/<sp
 		</div>
 	</section>
 	<!-- Services section end -->
-	<!-- Portfolio section start 
+	<!-- Portfolio section start -->
+	<!--
 	<section id="portfolio" class="darker">
 		<div class="container">
 			<header>
@@ -134,17 +147,19 @@ $landing_description = "We develop <span class='light-green'>websites</span>/<sp
 	<section id="contact">
 		<div class="container">
 			<header>
-				<h2>Contact us</h2>
+				<h2>Contact Me</h2>
 				<p>
-				Send us an email, we'll reply immediately!
+				Send me an email, I'll reply immediately!
 				</p>
 			</header>
+			<!--
 			<div class="map-wrapper">
 				<div class="map-canvas" id="map-canvas">106 Tommye Ln Tahlequah Oklahoma</div>
 				<div class="marker">
 					<span>We Are Here</span>
 				</div>
 			</div>
+			-->
 			<div class="row">
 				<div class="col-md-8">
 					<form class="row" name="contact-form" method="post" action="">
@@ -155,39 +170,32 @@ $landing_description = "We develop <span class='light-green'>websites</span>/<sp
 							<input name="mail" type="email" placeholder="Your Email" class="form-control" />
 						</div>
 						<div class="form-group col-md-12">
-							<input name="subject" type="text" placeholder="Type Message Here..." class="form-control" />
+							<input name="subject" type="text" placeholder="Subject" class="form-control" />
 						</div>
 						<div class="form-group col-md-12">
 							<textarea name="message" class="form-control" rows="10" placeholder="Message"></textarea>
 						</div>
 						<div class="form-group col-md-12">
-							<!--
-							<button class="btn btn-lg btn-primary">Send</button>
-							-->
-							<input type="submit" value="Send" class="btn btn-lg btn-primary">
+							<input type="submit" value="Send" name="submit" style="text-transform: lowercase;" class="btn btn-lg btn-primary">
 						</div>
 					</form>
 				</div>
 				<div class="col-md-3 col-md-offset-1">
 					<address>
-						<span>Our Address</span>
-						<p>
-							106 Tommye Ln<br>
-							Tahlequah, OK 74464<br>
-							United States
-						</p>
+						<span>Address</span>
+						<p><?php echo $ouraddress; ?></p>
 					</address>
 					<address>
 						<span>Email</span>
-						<p>jacksonconnerbrown@gmail.com</p>
+						<p><?php echo $ouremail; ?></p>
 					</address>
 					<address>
 						<span>Phone</span>
-						<p>+00 918-931-7486</p>
+						<p><?php echo $phone; ?></p>
 					</address>
 					<address>
 						<span>Skype</span>
-						<p>realtalk1217</p>
+						<p><?php echo $skype; ?></p>
 					</address>
 				</div>
 			</div>
@@ -195,11 +203,11 @@ $landing_description = "We develop <span class='light-green'>websites</span>/<sp
 	</section>
 	<!-- Contact section end -->
 	<!-- Footer start -->
-	<footer>
+	<footer class="foot">
 		<div class="container">
 			<div class="row">
 				<div class="col-md-8">
-					<p>&copy; Vaughn 2016</p>
+					<p>&copy; Jackson Brown 2016</p>
 				</div>
 				<div class="col-md-4">
 					<ul class="social-icons">
@@ -216,7 +224,7 @@ $landing_description = "We develop <span class='light-green'>websites</span>/<sp
 	<script type="text/javascript" src="js/jquery-1.11.2.min.js"></script>
 
 	<!-- Load Booststrap -->
-	<script type="text/javascript" src="js/bootstrap.js"></script>
+	<script type="text/javascript" src="js/bootstrap.js>"></script>
 
 	<script type="text/javascript" src="js/smooth-scroll.js"></script>
 
